34694a75e02a34cc95f07ad39a0c198b6e22d247
Dawidwolny123 <dw302225@student.polsl.pl>
2024-11-30 13:52:23 +0100

    push

--- hackaton2024/src/components/PageWithNavbar.jsx
+++ hackaton2024/src/components/PageWithNavbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Navbar"; // Import navbar z poprzedniego komponentu
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-
+import ButtonPanels from "./ButtonPanels";
 const PageWithNavbar = () => {
   return (
     <div>
@@ -13,22 +13,16 @@ const PageWithNavbar = () => {
       <Box
         sx={{
           backgroundColor: "#ffffff",
-          minHeight: "calc(100vh - 64px)", // Odejmujemy wysokość Navbar (64px domyślnie w Material-UI)
-          width: "100%", // Rozciągnięcie na całą szerokość
-          display: "flex", // Dodaje elastyczny układ
-          flexDirection: "column", // Ustawia kolumnowy układ
-          justifyContent: "center", // Wyśrodkowanie w pionie
-          alignItems: "center", // Wyśrodkowanie w poziomie
-          padding: "20px", // Dodaje odstępy
+          minHeight: "calc(100vh - 64px)", 
+          width: "100%", 
+          display: "flex", 
+          flexDirection: "column", 
+          justifyContent: "center", 
+          alignItems: "center",
+          padding: "20px",
         }}
       >
-        <Typography variant="h4" component="h1" sx={{ marginBottom: "20px" }}>
-          Witaj na mojej stronie!
-        </Typography>
-        <Typography variant="body1">
-          To jest przykładowy tekst na stronie. Możesz tutaj dodać dowolne
-          informacje, treść lub inne elementy, które chcesz wyświetlić.
-        </Typography>
+       <ButtonPanels></ButtonPanels>
       </Box>
     </div>
   );
