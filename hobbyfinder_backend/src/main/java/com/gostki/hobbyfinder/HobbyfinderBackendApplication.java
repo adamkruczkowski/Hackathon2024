d7460fef1e23ebf17c31db63e4d5965f36ca82a1
Michał Lach <ml300508@student.polsl.pl>
2024-11-30 14:35:43 +0100

    Initial project commit

---
+++ hobbyfinder_backend/src/main/java/com/gostki/hobbyfinder/HobbyfinderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gostki.hobbyfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyfinderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyfinderBackendApplication.class, args);
+	}
+
+}
---
+++ hobbyfinder_backend/src/test/java/com/gostki/hobbyfinder/HobbyfinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gostki.hobbyfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyfinderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
