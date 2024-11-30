2cc614111dceb60ad23dcae32e1ce3a3bc965055
Dawidwolny123 <dw302225@student.polsl.pl>
2024-11-30 18:13:06 +0100

    12344

--- hackaton2024/src/components/Navbar.jsx
+++ hackaton2024/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -6,13 +6,28 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Box from "@mui/material/Box";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const Navbar = () => {
-  return (
-    <Box sx={{ flexGrow: 1,
-      width: "100vw", // Zajmuje pełną szerokość ekranu
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
-     }}>
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        width: "100vw", // Zajmuje pełną szerokość ekranu
+      }}
+    >
       <AppBar position="static" sx={{ backgroundColor: "#1976d2", boxShadow: "none" }}>
         <Toolbar>
           <IconButton
@@ -21,9 +36,27 @@ const Navbar = () => {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleMenuOpen}
           >
             <MenuIcon />
           </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleMenuClose}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+          >
+            <MenuItem onClick={handleMenuClose}>Opcja 1</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Opcja 2</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Opcja 3</MenuItem>
+          </Menu>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: "bold" }}>
             My App
           </Typography>
